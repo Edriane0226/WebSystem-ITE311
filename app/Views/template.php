5aeab85881673181c051c4f5a2ee7499e7b7e447
Edriane0226 <edriane.bangonon26@gmail.com>
2025-08-02 20:53:42 +0800

    Bootstrap Integration Testing

---
+++ app/Views/template.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css">
+    <title>Navigation Bar</title>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
+         <a class="navbar-brand">Nav Sample</a>
+            <ul class="navbar-nav flex-row">
+                <li class="nav-item mx-2">
+                    <a href="#" class="nav-link active"> <i class="fa-brands fa-github"></i> </a>
+                </li>
+                <li class="nav-item mx-2">
+                    <a href="#" class="nav-link active"> <i class="fa-brands fa-reddit"></i> </a>
+                </li>
+                <li class="nav-item mx-2">
+                    <a href="#" class="nav-link active"> <i class="fa-brands fa-stack-overflow"></i> </a>
+                </li>
+                <li class="nav-item mx-4">
+                    <a href="#" class="nav-link active"> <i class="fas fa-home"></i> Home</a>
+                </li>
+                <li class="nav-item mx-4">
+                    <a href="#" class="nav-link active"> <i class="fas fa-box"></i> Package</a>
+                </li>
+                <li class="nav-item mx-4">
+                    <a href="#" class="nav-link active"> <i class="fa-solid fa-envelope"></i> Contact</a>
+                </li>
+                <li class="nav-item mx-4">
+                    <a href="#" class="nav-link active"> <i class="fa-solid fa-user"></i> </a>
+                </li>
+                </ul>
+
+        </div>
+    </nav>
+
+</body>
+</html>
