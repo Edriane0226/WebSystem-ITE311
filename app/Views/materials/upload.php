eccf0af2cd0a8f2efd6687ce513b6e493f8a4d01
Edriane0226 <edriane.bangonon26@gmail.com>
2025-12-04 11:52:59 +0800

    remove cdn link in upload form for material

--- app/Views/materials/upload.php
+++ app/Views/materials/upload.php
@@ -1,7 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Learning Materials</title>
 </head>
 <body class="bg-light">
